a60bbbbbc9c0573d75d84468de5d72d029ec5111
thefroshow75 <149398006+thefroshow75@users.noreply.github.com>
2024-10-16 18:52:18 -0400

    Fixed_Css

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function toggleDropdown() {
-      const projectList = document.getElementById('project-list');
-      projectList.classList.toggle('hidden');
-  }
-  
+let currentIndex = 0;
+const images = document.querySelectorAll('.banner img');
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+//setInterval(showNextImage, 3000); // Change image every 3 seconds
+
+
+// script.js
+document.getElementById('dropdownButton').addEventListener('click', function() {
+    var content = document.getElementById('dropdownContent');
+    content.classList.toggle('show');
+});
